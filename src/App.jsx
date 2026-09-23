5761dac892af11b7fd02e96eecb84f201eccd131
Husnain Ali <husnain22code@gmail.com>
2026-09-23 12:42:07 +0500

    Navbar work start

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Navbarone from './Component/Navbars/Navbar-one'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Navbarone/>
   )
 }
 
---
+++ src/Component/Navbars/Navbar-one.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbarone = () => {
+  return (
+    <div>hello</div>
+  )
+}
+
+export default Navbarone
